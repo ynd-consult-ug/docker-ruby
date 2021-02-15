c564a1fa175f87586a8de856760cc5dfd80ef59a
Tomasz Lagodzinski <gothrek22@users.noreply.github.com>
2021-02-15 09:59:48 +0100

    Switch from asdf to Make. (#3)
    * Move all to alpine 3.13, remove Centos, streamline everything.
    
    Co-authored-by: Tomasz Lagodzinski <tomasz.lagodzinski@ynd.co>

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,23 +2,13 @@
 
 IMAGE_BASENAME = 'yndconsult/docker-ruby:'
 RUBY_VERSIONS  = [
-  '2.5.3',
-  '2.6.2',
-  '2.6.3',
-  '2.6.5',
-  '2.6.6',
-  '2.7.2',
-  '3.0.0'
-]
-
-ALPINE_VERSIONS = [
-  '3.11',
-  '3.12',
-  '3.13'
-]
-
-CENTOS_VERSIONS = [
-  '7'
+  '2.5.3': '1cc9d0359a8ea35fc6111ec830d12e60168f3b9b305a3c2578357d360fcf306f',
+  '2.6.2': '91fcde77eea8e6206d775a48ac58450afe4883af1a42e5b358320beb33a445fa',
+  '2.6.3': '11a83f85c03d3f0fc9b8a9b6cad1b2674f26c5aaa43ba858d4b0fcc2b54171e1',
+  '2.6.5': 'd5d6da717fd48524596f9b78ac5a2eeb9691753da5c06923a6c31190abe01a62',
+  '2.6.6': '5db187882b7ac34016cd48d7032e197f07e4968f406b0690e20193b9b424841f',
+  '2.7.2': '1b95ab193cc8f5b5e59d2686cb3d5dcf1ddf2a86cb6950e0b4bdaae5040ec0d6',
+  '3.0.0': '68bfaeef027b6ccd0032504a68ae69721a70e97d921ff328c0c8836c798f6cb1'
 ]
 
 node('ynd') {
@@ -27,32 +17,16 @@ node('ynd') {
   }
 
   withDockerRegistry(credentialsId: 'hub.docker.com') {
-    ALPINE_VERSIONS.each{ os ->
-      RUBY_VERSIONS.each { version ->
-        IMAGE_NAME = "${IMAGE_BASENAME}${version}-alpine${os}"
-        stage(IMAGE_NAME) {
-          versionParts = version.tokenize('.')
-          sh "docker build -t ${IMAGE_NAME} -f Dockerfile.alpine" +
-          " --build-arg DISTRO_VERSION=${os}" +
-          " --build-arg RUBY_VERSION=${version} ."
-        }
-        stage("Push ${IMAGE_NAME}") {
-          sh "docker push ${IMAGE_NAME}"
-        }
+    RUBY_VERSIONS.each { version, sha ->
+      IMAGE_NAME = "${IMAGE_BASENAME}${version}"
+      stage(IMAGE_NAME) {
+        versionParts = version.tokenize('.')
+        sh "docker build -t ${IMAGE_NAME} -f Dockerfile.alpine" +
+        " --build-arg RUBY_SHA=${sha}" +
+        " --build-arg RUBY_VERSION=${version} ."
       }
-    }
-
-    CENTOS_VERSIONS.each{ os ->
-      RUBY_VERSIONS.each { version ->
-        IMAGE_NAME = "${IMAGE_BASENAME}${version}-centos${os}"
-        stage(IMAGE_NAME) {
-          sh "docker build -t ${IMAGE_NAME} -f Dockerfile.centos" +
-          " --build-arg DISTRO_VERSION=${os}" +
-          " --build-arg RUBY_VERSION=${version} ."
-        }
-        stage("Push ${IMAGE_NAME}") {
-          sh "docker push ${IMAGE_NAME}"
-        }
+      stage("Push ${IMAGE_NAME}") {
+        sh "docker push ${IMAGE_NAME}"
       }
     }
   }
---
+++ install.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+set -eux
+
+export RUBY_VERSION=$1
+RUBY_DOWNLOAD_SHA256=$2
+
+RUBY_MAJOR=$(echo ${RUBY_VERSION} | cut -d'.' -f -2 -)
+
+
+apk add --virtual .ruby-builddeps \
+		autoconf \
+		bison \
+		bzip2 \
+		bzip2-dev \
+		ca-certificates \
+		coreutils \
+		dpkg-dev dpkg \
+		gcc \
+		gdbm-dev \
+		glib-dev \
+		libc-dev \
+		libffi-dev \
+		libxml2-dev \
+		libxslt-dev \
+		linux-headers \
+		make \
+		ncurses-dev \
+		openssl \
+		openssl-dev \
+		patch \
+		procps \
+		readline-dev \
+		ruby \
+		tar \
+		xz \
+		yaml-dev \
+		zlib-dev
+
+
+wget -O ruby.tar.xz "https://cache.ruby-lang.org/pub/ruby/${RUBY_MAJOR%-rc}/ruby-$RUBY_VERSION.tar.xz"
+echo "$RUBY_DOWNLOAD_SHA256 *ruby.tar.xz" | sha256sum --check --strict
+
+mkdir -p /usr/src/ruby
+tar -xJf ruby.tar.xz -C /usr/src/ruby --strip-components=1
+rm ruby.tar.xz
+
+cd /usr/src/ruby
+
+wget -O 'thread-stack-fix.patch' 'https://bugs.ruby-lang.org/attachments/download/7081/0001-thread_pthread.c-make-get_main_stack-portable-on-lin.patch'
+echo '3ab628a51d92fdf0d2b5835e93564857aea73e0c1de00313864a94a6255cb645 *thread-stack-fix.patch' | sha256sum --check --strict
+patch -p1 -i thread-stack-fix.patch
+rm thread-stack-fix.patch
+
+{
+  echo '#define ENABLE_PATH_CHECK 0'
+  echo
+  cat file.c
+} > file.c.new
+mv file.c.new file.c
+
+autoconf
+gnuArch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)"
+# the configure script does not detect isnan/isinf as macros
+export ac_cv_func_isnan=yes ac_cv_func_isinf=yes
+./configure \
+  --build="$gnuArch" \
+  --disable-install-doc \
+  --enable-shared \
+
+make -j "$(nproc)"
+make install
+runDeps="$(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | sort -u | awk 'system("[ -e /usr/local/lib/" $1 " ]") == 0 { next } { print "so:" $1 }')"
+
+apk add --virtual .ruby-rundeps \
+  $runDeps \
+  bzip2 \
+  ca-certificates \
+  libffi-dev \
+  procps \
+  yaml-dev \
+  zlib-dev \
+  gmp-dev \
+  bash \
+  bc \
+  build-base \
+  busybox \
+  ca-certificates \
+  curl \
+  curl-dev \
+  gdbm-dev \
+  git \
+  glib \
+  graphviz \
+  jq \
+  less \
+  libgcc \
+  libintl \
+  libssl1.1 \
+  libstdc++ \
+  libx11 \
+  libxext \
+  libxml2 \
+  libxml2-dev \
+  libxrender \
+  libxslt \
+  libxslt-dev \
+  linux-headers \
+  musl-dev \
+  netcat-openbsd \
+  nodejs \
+  nodejs-npm \
+  openssh \
+  postgresql-client \
+  postgresql-dev \
+  readline-dev \
+  ttf-dejavu \
+  ttf-droid \
+  ttf-freefont \
+  ttf-liberation \
+  ttf-ubuntu-font-family \
+  vim \
+  yaml-dev \
+  yarn \
+
+apk del --no-network .ruby-builddeps
+
+cd /
+rm -rf /usr/src/ruby
+
+! apk --no-network list --installed | grep -v '^[.]ruby-rundeps' | grep -i ruby
+
+[ "$(command -v ruby)" = '/usr/local/bin/ruby' ]
+
+ruby --version
+gem --version
+bundle --version
